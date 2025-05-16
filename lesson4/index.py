b6702062919963e7ee9f4d53de6df4b31b2c5b58
yutingwen <yutingwen1208@gmail.com>
2025-05-16 10:49:04 +0800

    新增 Flask 應用程式，包含首頁、使用者頁面及產品頁面路由；新增 .gitignore 檔案以排除 __pycache__，並新增 CSS 檔案

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,1 +1,17 @@
-print("hello flask")
+'''print("hello flask") #evn 'flask' > lessson4在整合式終端機開啟 > python index.py => hello flask'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1><p>這是我的第1頁</p>"
+
+@app.route("/user")
+def user():
+    return "<h1>user</h1><p>這是我的第2頁</p>"
+
+@app.route("/product")
+def product():
+    return "<h1>product</h1><p>這是我的第3頁</p>"
